c0e76e14a2ea6c07a1cf4cb2e7b121a2c31e7909
Nate West <nayt@Nates-Air.lan>
2023-02-15 09:18:58 -0600

    initial commit

--- main.js
+++ main.js
@@ -30,26 +30,47 @@ const printStacks = () => {
 }
 
 // Next, what do you think this function should do?
-const movePiece = () => {
+const movePiece = (startStack, endStack) => {
   // Your code here
+ // pop() the last piece in startStack and push() to the endStack
+let piece = stacks.startStack.pop() //stacks[startStack]
 
+stacks.endStack.push(piece)
 }
 
 // Before you move, should you check if the move it actually allowed? Should 3 be able to be stacked on 2
 const isLegal = () => {
-  // Your code here
+  // 1. Move a startStack value to an empty endStack = true
+    // if endStack is empty, which means if .length = 0
 
+  // 2. Move a startStack value to an endStack if the last endStack value is a higher number = true
+
+  // 3. Move a startStack value to an endStack if the last endStack value is a lower number = false
+    // slice(-1) gets the last element of an array
+
+  // 4. Select a startStack that's empty = false
+    // if stacks.startStack.legnth = 0
+
+  // 5. Enter something besides a, b, c = false
+    // if !a or !b or !c
+// this function should return true or false
 }
 
 // What is a win in Towers of Hanoi? When should this function run?
 const checkForWin = () => {
-  // Your code here
-
+  // If B or C arrays = [4,3,2,1] you win!
+stacks.b.length = 4 // maybe stacks["b"]
 }
 
 // When is this function called? What should it do with its argument?
 const towersOfHanoi = (startStack, endStack) => {
-  // Your code here
+  // Take in the two arguments
+  // Check if isLegal
+    // if true, movePiece
+    // if false, display error message
+  // Call checkForWin
+    // if true, display win message
+    // if false, keep playing
 
 }
 
