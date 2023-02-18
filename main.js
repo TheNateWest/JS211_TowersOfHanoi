0655632d91d92f01b1c52f271e6b71019ed7c187
Nate West <southbynathanwest@yahoo.com>
2023-02-17 20:12:44 -0600

    we did it

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ const rl = readline.createInterface({
 let stacks = {
   a: [4, 3, 2, 1],
   b: [],
-  c: []
+  c: [],
 };
 
 // Start here. What is this function doing?
@@ -33,33 +33,85 @@ const printStacks = () => {
 const movePiece = (startStack, endStack) => {
   // Your code here
  // pop() the last piece in startStack and push() to the endStack
-let piece = stacks.startStack.pop() //stacks[startStack]
+ //stacks[startStack]
 
-stacks.endStack.push(piece)
+let piece = stacks[startStack].pop() 
+
+stacks[endStack].push(piece)
 }
 
+// if (condition) {
+//   run code
+// }
+
 // Before you move, should you check if the move it actually allowed? Should 3 be able to be stacked on 2
-const isLegal = () => {
-  // 1. Move a startStack value to an empty endStack = true
-    // if endStack is empty, which means if .length = 0
+const isLegal = (startStack, endStack) => {
 
-  // 2. Move a startStack value to an endStack if the last endStack value is a higher number = true
+   /**
+   * ORDER OF OPERATIONS:
+   * 1. check if inputs are a, b, or c
+   * 2. check if start is empty
+   * 3. check if end is empty
+   * 4. compare stacks
+   */
 
-  // 3. Move a startStack value to an endStack if the last endStack value is a lower number = false
-    // slice(-1) gets the last element of an array
+   let legalValues = ['a', 'b', 'c'];
 
-  // 4. Select a startStack that's empty = false
-    // if stacks.startStack.legnth = 0
+   if (legalValues.includes(startStack) && legalValues.includes(endStack)) {
+     if (stacks[startStack].length === 0) {
+      return false
+     }
+
+     if (stacks[endStack].length === 0) {
+      return true
+     }
+
+     if (stacks[startStack].slice(-1) < stacks[endStack].slice(-1)) {
+      return true
+     }
+
+     return false
+   }
+
+   return false
+
+
+
+
 
   // 5. Enter something besides a, b, c = false
     // if !a or !b or !c
-// this function should return true or false
+
+   
+      // 4. Select a startStack that's empty = false
+        // if stacks.startStack.legnth `= 0
+  
+
+  
+      // 1. Move a startStack value to an empty endStack = true
+      // if endStack is empty, which means if .length = 0
+
+      
+    // 2. Move a startStack value to an endStack if the last endStack value is a higher number = true
+
+  
+    // 3. Move a startStack value to an endStack if the last endStack value is a lower number = false
+      // slice(-1) gets the last element of an array
+  
+  
+  // this function should return true or false
+  
 }
 
 // What is a win in Towers of Hanoi? When should this function run?
 const checkForWin = () => {
   // If B or C arrays = [4,3,2,1] you win!
-stacks.b.length = 4 // maybe stacks["b"]
+if (stacks.b.length === 4 || stacks.c.length === 4) {
+  return true
+} else {
+  return false
+}
+
 }
 
 // When is this function called? What should it do with its argument?
@@ -68,10 +120,20 @@ const towersOfHanoi = (startStack, endStack) => {
   // Check if isLegal
     // if true, movePiece
     // if false, display error message
+  if (isLegal(startStack, endStack)) {
+    movePiece(startStack, endStack);
+
+  } else {
+    console.log('Illegal move');
+  }
+
+
   // Call checkForWin
     // if true, display win message
     // if false, keep playing
-
+  if (checkForWin()) {
+    console.log("Congrats bruh");
+  }
 }
 
 const getPrompt = () => {
